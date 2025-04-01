27a48935cce8bfee0e127a97aca5612663635462
j-somos <jsomos@protonmail.com>
2025-04-01 15:50:14 +0000

    created lab3 and lab4

---
+++ lab3/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace lab3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, lab3 World!");
+    }
+}
---
+++ lab4/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace lab4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, lab4 World!");
+    }
+}
